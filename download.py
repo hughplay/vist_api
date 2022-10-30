ef1ad7f2e2e4aec2a424d60a4468517747dafbfe
Mr.Blue <silverhugh.77@gmail.com>
2022-10-30 20:19:14 +0800

    update

--- download.py
+++ download.py
@@ -1,9 +1,8 @@
 # https://developers.google.com/drive/api/guides/api-specific-auth
 # https://developers.google.com/drive/api/quickstart/python
 # https://developers.google.com/drive/api/guides/manage-downloads
-from __future__ import print_function
-
-import os.path
+import hashlib
+import os
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -22,6 +21,17 @@ SCOPES = [
 OUT_PATH = "/data/reason/vist"
 if not os.path.exists(OUT_PATH):
     os.makedirs(OUT_PATH)
+
+
+def get_sha1_file_buffer(file_path):
+    sha1 = hashlib.sha1()
+    with open(file_path, "rb") as f:
+        while True:
+            data = f.read(65536)
+            if not data:
+                break
+            sha1.update(data)
+    return sha1.hexdigest()
 
 
 def main():
@@ -47,36 +57,46 @@ def main():
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
-    try:
-        service = build("drive", "v3", credentials=creds)
+    service = build("drive", "v3", credentials=creds)
 
-        with open("file_list.txt") as f:
-            lines = f.readlines()
-        items = [line.strip().split() for line in lines if line.strip()]
-        print("Files:")
-        for item in items:
-            print(item[0], item[1])
+    with open("file_list.txt") as f:
+        lines = f.readlines()
+    items = [line.strip().split() for line in lines if line.strip()]
+    print("Files:")
+    for item in items:
+        print(item[0], item[1])
 
-        print("start to download")
-        for item in items:
-            name, file_id = item
-            save_path = os.path.join(OUT_PATH, name)
+    print("\n-------\nstart to download\n-------")
+    for item in items:
+        name, file_id, sha1sum = item
+        save_path = os.path.join(OUT_PATH, name)
+        print(f"\nDownloading {name} to {save_path}")
+        while True:
             if os.path.exists(save_path):
-                print("skip", name)
-                continue
-            request = service.files().get_media(fileId=file_id)
-            with open(save_path, "wb") as fh:
-                downloader = MediaIoBaseDownload(fh, request)
-                done = False
-                while done is False:
-                    status, done = downloader.next_chunk()
+                print("File already exists, checking sha1sum...")
+                file_hash = get_sha1_file_buffer(save_path)
+                if file_hash == sha1sum:
+                    print(f"File {name} already exists and is correct.")
+                    break
+                else:
                     print(
-                        f"Download {name}: {status.progress() * 100 : .2f}%.",
-                        end="\r",
+                        f"The hash is not correct. Download {name} again."
                     )
-    except HttpError as error:
-        # TODO(developer) - Handle errors from drive API.
-        print(f"An error occurred: {error}")
+                    os.remove(save_path)
+            try:
+                service_files = service.files()
+                request = service_files.get_media(fileId=file_id)
+                with open(save_path, "wb") as fh:
+                    downloader = MediaIoBaseDownload(fh, request)
+                    done = False
+                    while done is False:
+                        status, done = downloader.next_chunk(num_retries=10)
+                        print(
+                            f"Download {name}: {status.progress() * 100 : .2f}%.",
+                            end="\r",
+                        )
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
